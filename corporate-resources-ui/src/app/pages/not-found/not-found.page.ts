6d6f1581b687c3b8f7bbdf7ff91c276109a4e545
Ruan Carlos da Silva <143547755+ruansilva123@users.noreply.github.com>
2025-08-17 03:41:09 +0000

    feat: (UI) added a reservations page content

--- corporate-resources-ui/src/app/pages/not-found/not-found.page.ts
+++ corporate-resources-ui/src/app/pages/not-found/not-found.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
     selector: 'app-not-found',
--- corporate-resources-ui/src/app/pages/reservations/reservations.page.ts
+++ corporate-resources-ui/src/app/pages/reservations/reservations.page.ts
@@ -1,17 +1,45 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
 import { HeaderComponent } from '../../shared/components/header/header.component';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-reservations',
     standalone: true,
     imports: [
         FooterComponent,
-        HeaderComponent
+        HeaderComponent,
+        FormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatDatepickerModule,
+        MatTableModule,
+        MatIconModule,
+        MatTooltipModule
     ],
+    providers: [provideNativeDateAdapter()],
     templateUrl: './reservations.page.html',
     styleUrl: './reservations.page.scss'
 })
 export class ReservationsPage {
+    private router: Router;
 
+    protected displayedColumns: string[] = ['Item', 'Category', 'City', 'LoanedTo', 'StartReservation', 'EndReservation'];
+    protected dataSource = [];
+
+    constructor(router: Router) {
+        this.router = router;
+    }
+
+    protected goToReservationForm() {
+        this.router.navigate(['/reservations/create']);
+    }
 }
